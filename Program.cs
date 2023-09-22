68e82d5b919fd26ad816d4e1eeaebf7bb145179e
Clone888 <dwclone@gmail.com>
2023-09-22 09:23:33 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Concurrent;
+
+string[] list = File.ReadAllLines("../../../Husdjur.csv");
+
+Dictionary<string, int> animal = new Dictionary<string, int>();
+
+List<int> petAge = new List<int>(); // Skapar listan 'petAge'
+
+foreach (string line in list)
+{
+    string[] split = line.Split(",");
+
+    if (int.TryParse(split[1], out int age))
+    {
+        string petName = split[0].ToLower();
+        animal.Add(petName, age);   // Lägger till 'petName' och 'age' i dictionary 'animal'
+        petAge.Add(age);    // Lägger till 'age' i listan 'petAge'
+    }
+    else
+    {
+        Console.WriteLine("Something went wrong");
+    }
+}
+
+//petAge.Sort(); // Funkar att sortera här, men hur göra med en 'for'-loop?
+
+
+
+for (int i = 0; i < petAge.ToArray().Length; i++)
+{
+    for (int j = 0; j < petAge.ToArray().Length - 1; j++)
+    {
+
+        int first = petAge[j];
+        int second = petAge[j + 1];
+
+        if (first > second)
+        {
+            petAge[j] = second;     
+            petAge[j + 1] = first;              
+        }
+    }
+}
+
+
+
+foreach (int line in petAge)
+{
+    Console.Write(line + ", ");
+}
+
+//File.WriteAllLines("../../../sorteradeDjur.txt"); // Använda hur????
+
+
+/*
+
+
+int temp = 0;
+
+
+for (int i = 0; i < petAge.ToArray().Length; i++)
+{
+
+    for (int j = 0; j < petAge.ToArray().Length - 1; j++)
+    {
+        int first = petAge[i];
+        int second = petAge[i + 1];
+        if (first > second)
+        {
+
+            first = second;
+            second = first;
+            
+
+        }
+
+
+    }
+    
+}
+
+Console.WriteLine(petAge);
+
+
+*/
+
+
+/*
+
+Console.WriteLine();
+Console.WriteLine("Choose a name: ");
+
+
+
+bool isRunning = true;
+
+string? input = Console.ReadLine().ToLower();
+
+while (isRunning)
+{
+    if (animal.ContainsKey(input))
+    {
+        Console.WriteLine("Birthyear of " + input + " is " + (2023 - animal[input]));
+        isRunning = false;
+    }
+    else
+    {
+        Console.WriteLine("Name dosent exist, try again");
+        isRunning = true;
+        input = Console.ReadLine().ToLower();
+
+    }
+}
+
+
+
+
+//Console.WriteLine(split[0]);
+
+
+*/
